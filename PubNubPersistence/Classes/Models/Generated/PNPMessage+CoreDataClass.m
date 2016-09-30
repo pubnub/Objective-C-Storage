b4274f1a2e9dd2700b52083581d4208f6bee91d3
Jordan Zucker <jordan.zucker@gmail.com>
2016-09-30 16:17:10 -0700

    Just saving a little more

--- PubNubPersistence/Classes/Models/Generated/PNPMessage+CoreDataClass.m
+++ PubNubPersistence/Classes/Models/Generated/PNPMessage+CoreDataClass.m
@@ -17,4 +17,41 @@
     self.creationDate = [NSDate date];
 }
 
+/*
+- (BOOL)validateAge:(id*)ioValue error:(NSError**)outError
+{
+    if (*ioValue == nil) {
+        return YES;
+    }
+    if ([*ioValue floatValue] <= 0.0) {
+        if (outError == NULL) {
+            return NO;
+        }
+        NSString *errorStr = NSLocalizedStringFromTable(@"Age must be greater than zero", @"Employee", @"validation: zero age error");
+        NSDictionary *userInfoDict = @{NSLocalizedDescriptionKey: errorStr};
+        NSError *error = [[NSError alloc] initWithDomain:EMPLOYEE_ERROR_DOMAIN code:PERSON_INVALID_AGE_CODE userInfo:userInfoDict];
+        *outError = error;
+        return NO;
+    } else {
+        return YES;
+    }
+}
+ */
+
+/*
+- (BOOL)validatePayload:(id *)ioValue error:(NSError **)outError {
+    if (*ioValue == nil) {
+        if (outError == NULL) {
+            return NO;
+        }
+        NSString *errorStr = [NSString stringWithFormat:@"payload cannot be nil"];
+        NSDictionary *userInfoDict = @{NSLocalizedDescriptionKey: errorStr};
+        NSError *error = [[NSError alloc] initWithDomain:@"PubNubPersistence" code:200 userInfo:userInfoDict];
+        *outError = error;
+        return NO;
+    }
+    return YES;
+}
+ */
+
 @end
